059ce5efb5b765722a69fd72a459a9d42b5a90f5
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-02-28 11:12:29 -0800

    created working reset button

--- stopwatch/main.js
+++ stopwatch/main.js
@@ -2,17 +2,22 @@
 
 var startButton = document.querySelector('button.start')
 var pauseButton = document.querySelector('button.pause')
+var resetButton = document.querySelector('button.reset')
 var interval
+var timer = {
+  seconds: 0
+}
 
 startButton.addEventListener('click', function () {
-  var timer = {
-    seconds: 0
-  }
   startTimer(timer, document.querySelector('p.timer'))
 })
 
 pauseButton.addEventListener('click', function() {
   clearInterval(interval)
+})
+
+resetButton.addEventListener('click', function() {
+  resetTime(document.querySelector('p.timer'))
 })
 
 function startTimer(timer, location) {
@@ -26,4 +31,9 @@ function readTime(timePassed) {
   var minutes = Math.floor(timePassed.seconds / 60)
   var seconds = timePassed.seconds - minutes * 60
   return minutes + ':' + seconds
+}
+
+function resetTime(location) {
+  location.textContent = '0:0'
+  timer.seconds = 0
 }
